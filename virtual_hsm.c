707b877ceb2e11914a8c91b63bbb1fe4c422bd02
Alon Hillel-Tuch <ah5647@nyu.edu>
2024-09-30 17:39:23 -0400

    security updates
    out of bounds, rotation features, some errors included to test actions unit tests

--- virtual_hsm.c
+++ virtual_hsm.c
@@ -10,9 +10,10 @@
 #define KEY_SIZE 32
 #define IV_SIZE 16
 #define MAX_FILENAME 256
+#define MAX_NAME_LENGTH 49
 
 typedef struct {
-    char name[50];
+    char name[MAX_NAME_LENGTH + 1];
     unsigned char encrypted_key[KEY_SIZE + EVP_MAX_BLOCK_LENGTH];
     unsigned char iv[IV_SIZE];
     int encrypted_len;
@@ -27,7 +28,7 @@ char master_key_file[MAX_FILENAME] = "master.key";
 
 void handle_errors() {
     ERR_print_errors_fp(stderr);
-    abort();
+    exit(1);
 }
 
 void generate_master_key() {
@@ -41,24 +42,48 @@ void generate_master_key() {
         printf("%02x", new_master_key[i]);
     }
     printf("\n");
+
+    FILE *file = fopen(master_key_file, "wb");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Unable to open master key file for writing.\n");
+        exit(1);
+    }
+    fwrite(new_master_key, 1, KEY_SIZE, file);
+    fclose(file);
+
+    printf("WARNING: The master key has been stored in %s. This is insecure and should only be used for educational purposes.\n", master_key_file);
 }
 
 void load_master_key() {
     FILE *file = fopen(master_key_file, "rb");
     if (file == NULL) {
-        if (RAND_bytes(master_key, KEY_SIZE) != 1) {
-            handle_errors();
+        printf("Master key file not found. Please enter the master key in hex format: ");
+        char hex_key[KEY_SIZE * 2 + 1];
+        if (fgets(hex_key, sizeof(hex_key), stdin) == NULL) {
+            fprintf(stderr, "Error reading master key.\n");
+            exit(1);
         }
-        file = fopen(master_key_file, "wb");
-        fwrite(master_key, 1, KEY_SIZE, file);
+        
+        for (int i = 0; i < KEY_SIZE; i++) {
+            sscanf(hex_key + 2*i, "%2hhx", &master_key[i]);
+        }
     } else {
-        fread(master_key, 1, KEY_SIZE, file);
+        if (fread(master_key, 1, KEY_SIZE, file) != KEY_SIZE) {
+            fprintf(stderr, "Error: Invalid master key file.\n");
+            fclose(file);
+            exit(1);
+        }
+        fclose(file);
+        printf("WARNING: Master key loaded from file. This is insecure and should only be used for educational purposes.\n");
     }
-    fclose(file);
 }
 
 void save_keystore() {
     FILE *file = fopen(keystore_file, "wb");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening keystore file for writing.\n");
+        exit(1);
+    }
     fwrite(&key_count, sizeof(int), 1, file);
     fwrite(keystore, sizeof(KeyEntry), key_count, file);
     fclose(file);
@@ -82,7 +107,7 @@ int encrypt_key(const unsigned char *plaintext, unsigned char *ciphertext, int *
 
     if (!(ctx = EVP_CIPHER_CTX_new())) handle_errors();
 
-    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, master_key, iv))
+    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, master_key, iv))
         handle_errors();
 
     int len;
@@ -103,7 +128,7 @@ int decrypt_key(const unsigned char *ciphertext, int ciphertext_len, unsigned ch
 
     if (!(ctx = EVP_CIPHER_CTX_new())) handle_errors();
 
-    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, master_key, iv))
+    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, master_key, iv))
         handle_errors();
 
     int len;
@@ -125,6 +150,11 @@ void store_key(const char *name, const unsigned char *key) {
         exit(1);
     }
 
+    if (strlen(name) > MAX_NAME_LENGTH) {
+        fprintf(stderr, "Error: Key name is too long.\n");
+        exit(1);
+    }
+
     for (int i = 0; i < key_count; i++) {
         if (strcmp(keystore[i].name, name) == 0) {
             fprintf(stderr, "Error: Key with this name already exists.\n");
@@ -133,7 +163,8 @@ void store_key(const char *name, const unsigned char *key) {
     }
 
     KeyEntry *entry = &keystore[key_count++];
-    strncpy(entry->name, name, sizeof(entry->name) - 1);
+    strncpy(entry->name, name, MAX_NAME_LENGTH);
+    entry->name[MAX_NAME_LENGTH] = '\0';
     if (!encrypt_key(key, entry->encrypted_key, &entry->encrypted_len, entry->iv)) {
         fprintf(stderr, "Error: Failed to encrypt key.\n");
         exit(1);
@@ -168,21 +199,9 @@ void list_keys() {
     }
 }
 
-int set_master_key_from_hex(const char *hex_key) {
-    if (strlen(hex_key) != KEY_SIZE * 2) {
-        fprintf(stderr, "Error: Invalid master key length. Expected %d characters.\n", KEY_SIZE * 2);
-        return 0;
-    }
-
-    for (int i = 0; i < KEY_SIZE; i++) {
-        sscanf(hex_key + 2*i, "%2hhx", &master_key[i]);
-    }
-    return 1;
-}
-
 void print_usage() {
     fprintf(stderr, "Usage:\n");
-    fprintf(stderr, "  ./virtual_hsm [-keystore <keystore_file>] [-master <master_key_file>] [-master_key <hex_key>] <command> [options]\n");
+    fprintf(stderr, "  ./virtual_hsm [-keystore <keystore_file>] <command> [options]\n");
     fprintf(stderr, "Commands:\n");
     fprintf(stderr, "  -store <key_name>\n");
     fprintf(stderr, "  -retrieve <key_name> [-pipe]\n");
@@ -192,18 +211,13 @@ void print_usage() {
 
 int main(int argc, char *argv[]) {
     int i;
-    int master_key_set = 0;
     for (i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-keystore") == 0 && i + 1 < argc) {
             strncpy(keystore_file, argv[++i], MAX_FILENAME - 1);
+            keystore_file[MAX_FILENAME - 1] = '\0';
         } else if (strcmp(argv[i], "-master") == 0 && i + 1 < argc) {
             strncpy(master_key_file, argv[++i], MAX_FILENAME - 1);
-        } else if (strcmp(argv[i], "-master_key") == 0 && i + 1 < argc) {
-            if (set_master_key_from_hex(argv[++i])) {
-                master_key_set = 1;
-            } else {
-                return 1;
-            }
+            master_key_file[MAX_FILENAME - 1] = '\0';
         } else {
             break;
         }
@@ -219,9 +233,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    if (!master_key_set) {
-        load_master_key();
-    }
+    load_master_key();
     load_keystore();
 
     if (strcmp(argv[i], "-store") == 0) {
