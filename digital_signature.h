194c7a800f4c76668cd642e08b8526be287111a5
Alon Hillel-Tuch <ah5647@nyu.edu>
2024-10-17 20:12:23 -0400

    digital signature functionality
    New Functionality:
    
    Key pair generation using Ed25519 algorithm
    Digital signature creation
    Signature verification
    Public key export in PEM format
    Public key import from PEM format

---
+++ digital_signature.h
@@ -0,0 +1,177 @@
+#ifndef DIGITAL_SIGNATURE_H
+#define DIGITAL_SIGNATURE_H
+
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+#include <string.h>
+
+#define MAX_SIGNATURE_SIZE 256
+#define SHA256_DIGEST_LENGTH 32
+
+// Structure to hold a key pair
+typedef struct {
+    EVP_PKEY *pkey;
+    char name[MAX_NAME_LENGTH + 1];
+} KeyPair;
+
+// Function prototype declarations
+int generate_key_pair(const char *name);
+int sign_data(const char *key_name, const unsigned char *data, size_t data_len, unsigned char *signature, size_t *sig_len);
+int verify_signature(const char *key_name, const unsigned char *data, size_t data_len, const unsigned char *signature, size_t sig_len);
+int export_public_key(const char *key_name, char **pem_key);
+int import_public_key(const char *name, const char *pem_key);
+
+// Helper function to find a key pair by name
+KeyPair *find_key_pair(const char *name) {
+    for (int i = 0; i < key_count; i++) {
+        if (strcmp(keystore[i].name, name) == 0) {
+            // Decrypt the key
+            unsigned char decrypted_key[KEY_SIZE];
+            int decrypted_len = decrypt_key(keystore[i].encrypted_key, 
+                                            keystore[i].encrypted_len,
+                                            decrypted_key, 
+                                            keystore[i].iv,
+                                            keystore[i].tag);
+            
+            if (decrypted_len != KEY_SIZE) {
+                return NULL;
+            }
+            
+            // Convert the decrypted key to an EVP_PKEY structure
+            EVP_PKEY *pkey = EVP_PKEY_new_raw_private_key(EVP_PKEY_ED25519, NULL, decrypted_key, KEY_SIZE);
+            if (!pkey) {
+                return NULL;
+            }
+            
+            KeyPair *pair = malloc(sizeof(KeyPair));
+            pair->pkey = pkey;
+            strncpy(pair->name, name, MAX_NAME_LENGTH);
+            pair->name[MAX_NAME_LENGTH] = '\0';
+            
+            return pair;
+        }
+    }
+    return NULL;
+}
+
+// Generate a new Ed25519 key pair and store it in the HSM
+int generate_key_pair(const char *name) {
+    EVP_PKEY *pkey = NULL;
+    EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_ED25519, NULL);
+    
+    if (!pctx || EVP_PKEY_keygen_init(pctx) <= 0 || EVP_PKEY_keygen(pctx, &pkey) <= 0) {
+        EVP_PKEY_CTX_free(pctx);
+        return 0;
+    }
+    
+    EVP_PKEY_CTX_free(pctx);
+    
+    size_t key_len = KEY_SIZE;
+    unsigned char key_data[KEY_SIZE];
+    
+    if (EVP_PKEY_get_raw_private_key(pkey, key_data, &key_len) <= 0 || key_len != KEY_SIZE) {
+        EVP_PKEY_free(pkey);
+        return 0;
+    }
+    
+    store_key(name, key_data);
+    EVP_PKEY_free(pkey);
+    return 1;
+}
+
+// Sign data using a key stored in the HSM
+int sign_data(const char *key_name, const unsigned char *data, size_t data_len, unsigned char *signature, size_t *sig_len) {
+    KeyPair *pair = find_key_pair(key_name);
+    if (!pair) {
+        return 0;
+    }
+    
+    EVP_MD_CTX *md_ctx = EVP_MD_CTX_new();
+    if (!md_ctx) {
+        return 0;
+    }
+    
+    if (EVP_DigestSignInit(md_ctx, NULL, NULL, NULL, pair->pkey) <= 0) {
+        EVP_MD_CTX_free(md_ctx);
+        return 0;
+    }
+    
+    if (EVP_DigestSign(md_ctx, signature, sig_len, data, data_len) <= 0) {
+        EVP_MD_CTX_free(md_ctx);
+        return 0;
+    }
+    
+    EVP_MD_CTX_free(md_ctx);
+    return 1;
+}
+
+// Verify a signature using a key stored in the HSM
+int verify_signature(const char *key_name, const unsigned char *data, size_t data_len, const unsigned char *signature, size_t sig_len) {
+    KeyPair *pair = find_key_pair(key_name);
+    if (!pair) {
+        return 0;
+    }
+    
+    EVP_MD_CTX *md_ctx = EVP_MD_CTX_new();
+    if (!md_ctx) {
+        return 0;
+    }
+    
+    if (EVP_DigestVerifyInit(md_ctx, NULL, NULL, NULL, pair->pkey) <= 0) {
+        EVP_MD_CTX_free(md_ctx);
+        return 0;
+    }
+    
+    int ret = EVP_DigestVerify(md_ctx, signature, sig_len, data, data_len);
+    
+    EVP_MD_CTX_free(md_ctx);
+    return (ret == 1);
+}
+
+// Export the public key in PEM format
+int export_public_key(const char *key_name, char **pem_key) {
+    KeyPair *pair = find_key_pair(key_name);
+    if (!pair) {
+        return 0;
+    }
+    
+    BIO *bio = BIO_new(BIO_s_mem());
+    if (!bio || PEM_write_bio_PUBKEY(bio, pair->pkey) <= 0) {
+        BIO_free(bio);
+        return 0;
+    }
+    
+    long pem_size = BIO_get_mem_data(bio, pem_key);
+    *pem_key = malloc(pem_size + 1);
+    BIO_read(bio, *pem_key, pem_size);
+    (*pem_key)[pem_size] = '\0';
+    
+    BIO_free(bio);
+    return 1;
+}
+
+// Import a public key in PEM format
+int import_public_key(const char *name, const char *pem_key) {
+    BIO *bio = BIO_new_mem_buf(pem_key, -1);
+    EVP_PKEY *pkey = PEM_read_bio_PUBKEY(bio, NULL, NULL, NULL);
+    BIO_free(bio);
+    
+    if (!pkey) {
+        return 0;
+    }
+    
+    unsigned char key_data[KEY_SIZE];
+    size_t key_len = sizeof(key_data);
+    
+    if (EVP_PKEY_get_raw_public_key(pkey, key_data, &key_len) <= 0 || key_len != KEY_SIZE) {
+        EVP_PKEY_free(pkey);
+        return 0;
+    }
+    
+    store_key(name, key_data);
+    EVP_PKEY_free(pkey);
+    return 1;
+}
+
+#endif // DIGITAL_SIGNATURE_H
