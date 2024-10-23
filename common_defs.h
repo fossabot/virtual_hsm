3df8943a4bfd80621758e14149f5496efd320835
Alon Hillel-Tuch <ah5647@nyu.edu>
2024-10-22 23:37:32 -0400

    Create common_defs.h
    A common config and definition file for the HSM

---
+++ common_defs.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_DEFS_H
+#define COMMON_DEFS_H
+
+#define MAX_FILENAME 256
+#define MAX_NAME_LENGTH 49
+#define MAX_KEYS 100
+#define KEY_SIZE 32 
+#define IV_SIZE 12  
+#define TAG_SIZE 16
+
+#endif // COMMON_DEFS_H
