f3706c30d0e5c6192b62178d3b10d73db2baaac9
Alon Hillel-Tuch <ah5647@nyu.edu>
2024-11-05 10:52:14 -0500

    Update utils.h - new input file functions

--- utils.h
+++ utils.h
@@ -10,6 +10,9 @@
 int hex_to_int(char c);
 void hex_to_bytes(const char *hex, unsigned char *bytes, size_t length);
 void handle_errors(void);
+unsigned char* read_file(const char* filename, size_t* file_size);
+int write_file(const char* filename, const unsigned char* data, size_t data_len);
+size_t read_hex_string(const char* hex_string, unsigned char* buffer, size_t buffer_size);
 
 int hex_to_int(char c) {
     if (c >= '0' && c <= '9') return c - '0';
@@ -32,6 +35,57 @@ void handle_errors() {
     fprintf(stderr, "Debug: OpenSSL Error: %s\n", err_msg);
     ERR_print_errors_fp(stderr);
     exit(1);
+}
+
+unsigned char* read_file(const char* filename, size_t* file_size) {
+    FILE* fp = fopen(filename, "rb");
+    if (!fp) {
+        return NULL;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    long length = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    unsigned char* buffer = malloc((size_t)length);
+    if (!buffer) {
+        fclose(fp);
+        return NULL;
+    }
+
+    *file_size = fread(buffer, 1, (size_t)length, fp);
+    fclose(fp);
+    return buffer;
+}
+
+int write_file(const char* filename, const unsigned char* data, size_t data_len) {
+    FILE* fp = fopen(filename, "wb");
+    if (!fp) {
+        return 0;
+    }
+
+    size_t written = fwrite(data, 1, data_len, fp);
+    fclose(fp);
+    return written == data_len;
+}
+
+size_t read_hex_string(const char* hex_string, unsigned char* buffer, size_t buffer_size) {
+    size_t len = strlen(hex_string);
+    if (len > buffer_size * 2) {
+        len = buffer_size * 2;
+    }
+
+    size_t j = 0;
+    for (size_t i = 0; i < len; i += 2) {
+        int high = hex_to_int(hex_string[i]);
+        int low = hex_to_int(hex_string[i + 1]);
+        if (high == -1 || low == -1) {
+            return 0;
+        }
+        buffer[j++] = (high << 4) | low;
+    }
+
+    return j;
 }
 
 #endif // UTILS_H
